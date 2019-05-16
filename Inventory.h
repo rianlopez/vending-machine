e4897b76c607b433a00be5c0d391fa413412f318
Rian Markus Lopez <lopezrianmarkus@gmail.com>
2019-05-15 21:32:59 -0700

    Added comments and parameter names

--- Inventory.h
+++ Inventory.h
@@ -4,6 +4,9 @@
 #define INVENTORY_H
 
 #include "Item.h"
+#include <iostream>
+
+using namespace std;
 
 class Inventory
 {
@@ -12,16 +15,17 @@ private:
     Item *pItem;
 
 public:
-    Inventory(int size = 0, Item *ptr = nullptr);
+    Inventory(int size = 0, Item *ptr = nullptr); // Item *ptr can pass an array
+                                                  // of Item objects
     Inventory(const Inventory &);
     ~Inventory();
     void addItem(const Item &);
-    void removeItem(int);
-    void setItemList(const Item [], int);
+    void removeItem(int index);
+    void setItemList(const Item [], int size);
     int getSize() const;
     int search(const Item &) const;
     Inventory &operator=(const Inventory &);
-    Item &operator[](int);
+    Item &operator[](int index);
 
 };
 
