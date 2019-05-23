4517b67e5c38b0708aa20da89762efa97d8d694c
Rian Markus Lopez <lopezrianmarkus@gmail.com>
2019-05-23 13:40:46 -0700

    Updated abstract class

--- Machine.h
+++ Machine.h
@@ -4,9 +4,9 @@
 #define MACHINE_H
 
 #include "Inventory.h"
+#include <iostream>
+#include <fstream>
 #include <iomanip>
-
-using namespace std;
 
 // in cents
 const int QUARTER = 25;
@@ -16,11 +16,10 @@ const int NICKEL = 5;
 class Machine
 {
 private:
-
 	struct Option
 	{
-		string code;
-		Item *pItem; // points to an item inside the machine's inventory
+		std::string code;
+		Inventory::Item *pItem; // points to an item inside the machine's inventory
 	};
 
 	// initial amount of each coin
@@ -33,34 +32,33 @@ private:
     int nickel;
 	int purchaseCount; // number of valid transactions
     int productCount;
+	std::string name;
+	Inventory products;
 	Option *pOption;
-    Inventory products;
-	Inventory *pInv;   // points to a shared inventory
 	virtual void printPaymentType() const = 0;
+	virtual bool payment() const = 0;
 
 public:
     Machine(int q = 0, int d = 0, int n = 0);
+	Machine(const std::string &);
     Machine(const Machine &);
 	~Machine();
+	void setInitQuarter(int);
+	void setInitDime(int);
+	void setInitNickel(int);
+	void setInitCoins(int q, int d, int n);
     void setQuarter(int);
     void setDime(int);
     void setNickel(int);
     void setCoins(int q, int d, int n);
-	void setSharedInv(Inventory *);
-	void addProduct(const Item &, int qty = 1);
-	int getInitQuarter() const;
-	int getInitDime() const;
-	int getInitNickel() const;
-    int getQuarter() const;
-    int getDime() const;
-    int getNickel() const;
-	int getPurchaseCount() const;
-	int getProductCount() const;
-	double getInitBalance() const;
-	double getCurrBalance() const;
+	void setName(const std::string &);
+	void addProduct(const std::string & code ,int id, int qty, int price, const std::string &desc);
+	void addProduct(const std::string code,const Inventory::Item &);
+	void purchase();
+	void print(const std::string &fileName) const;
+	void print(std::ofstream &outFile);
+	std::string getName() const;
 	Machine &operator=(const Machine &);
-
-	friend ostream &operator<<(ostream, const Machine &);
 
 };
 
