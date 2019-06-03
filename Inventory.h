2f5dc1c5364cb99ac1a28eeb9ab15648eb7263aa
Rian Markus Lopez <lopezrianmarkus@gmail.com>
2019-06-03 01:20:21 -0700

    Added comments

--- Inventory.h
+++ Inventory.h
@@ -9,7 +9,7 @@ class Inventory
 {
 private:
 	int size;
-	Item *pItem;
+	Item *pItem; // dynamic array of Item objects
 
 public:
 	Inventory();
@@ -20,7 +20,9 @@ public:
 	void addItem(const Item &);
 	void setItemList(const Item[], int size);
 	int getSize() const { return size; }
-	Item getItem(int id, int qty = 0);
+	Item getItem(int id, int qty = 0); // searches an Item object by id and if found,
+									   // returns the Item object with the available
+									   // quantity
 	Inventory &operator=(const Inventory &);
 	Item &operator[](int index) { return pItem[index]; }
 	const Item &operator[](int index) const { return pItem[index]; }
